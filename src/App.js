134f37f7a681b331f4d3ee024eed547e3d124e67
Jackson Cai <jcai8649@gmail.com>
2021-02-25 12:39:11 -0800

    Add SVG logo to homepage and header component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { Route, Switch } from "react-router";
 import "./App.css";
 import HomePage from "./pages/Homepage/HomePage";
 import ShopPage from "./pages/Shop/Shop";
+import Header from "./components/Header/Header";
 
 function App() {
   const HatPage = () => <div>HatPage</div>;
 
   return (
     <div>
+      <Header />
       <Switch>
-        <Route path="/shop" component={ShopPage} />
         <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
