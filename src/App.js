60522828a470eb53e70745d85238851cff091959
Jackson Cai <jcai8649@gmail.com>
2021-02-26 12:39:11 -0800

    Create sign in form with customom input and button

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import { Route, Switch } from "react-router";
 import "./App.css";
 import HomePage from "./pages/Homepage/HomePage";
-import ShopPage from "./pages/Shop/Shop";
+import ShopPage from "./pages/Shop/ShopPage";
 import Header from "./components/Header/Header";
+import SignInAndSignUpPage from "./pages/SignInAndSignUpPage/SignInAndSignUpPage";
 
 function App() {
-  const HatPage = () => <div>HatPage</div>;
-
   return (
     <div>
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
