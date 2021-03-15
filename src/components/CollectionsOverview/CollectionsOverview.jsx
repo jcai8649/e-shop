401e2e2fb20fcccdde11bc6ba3b841ea2c0a24d1
Jackson Cai <jcai8649@gmail.com>
2021-03-15 12:39:11 -0800

    Adjust collection preview selector for data normalization

--- src/components/CollectionsOverview/CollectionsOverview.jsx
+++ src/components/CollectionsOverview/CollectionsOverview.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop.selector";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
 import CollectionPreview from "../CollectionPreview/CollectionPreview";
 import "./CollectionsOverview.styles.scss";
 const CollectionOverview = ({ collections }) => (
@@ -13,7 +13,7 @@ const CollectionOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
